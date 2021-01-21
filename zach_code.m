68c8518d90bc877cfe65a53a106e01c5215c722f
zach2898 <77704031+zach2898@users.noreply.github.com>
2021-01-21 15:41:28 -0700

    Update zach_code.m

--- zach_code.m
+++ zach_code.m
@@ -6,6 +6,7 @@ g = 9.8;
 t = linspace(0,3*pi/4,100);
 R = 50;
 r = 5;
+lR = 15;
 %Element 1 - valley
 x = -R*cos(t);
 y = 0.*t;
@@ -26,17 +27,23 @@ y3 = 0.*t3 + y2(end);
 z3 = -r*sin(t3)+z2(end)+sin(pi/4)*r;
 
 %Straight section
-t4 = linspace(0,5,20);
+t4 = linspace(0,100,100);
 x4 = t4 + x3(end);
 y4 = 0.*t4 + y3(end);
 z4 = 0.*t4 + z3(end);
 
+%Loop
+t5 = linspace(0,2*pi,100);
+x5 = lR*sin(t5) + x4(end);
+y5 = (1/(2*pi)).*t5 + y4(end);
+z5 = -lR*cos(t5) + z4(end) + lR;
 
 scatter3(x,y,z)
 hold on;
 scatter3(x2,y2,z2)
 scatter3(x3,y3,z3)
 scatter3(x4,y4,z4)
+scatter3(x5,y5,z5)
 function v = getSpeed(h,h0)
     v = sqrt(2*9.8*(h0-h));
 end
