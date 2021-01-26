6f7c13b812cd1b5bf00ac5eef17dd02d2908e267
zach2898 <77704031+zach2898@users.noreply.github.com>
2021-01-26 15:19:12 -0700

    Add files via upload

--- rollercoaster.m
+++ rollercoaster.m
@@ -6,8 +6,9 @@ m = 1;
 g = 9.8;
 t = linspace(0,3*pi/4,100);
 R = 50;
-r = 25;
-lR = 15;
+r = 45;
+r2 = 15;
+lR = 20;
 tR = 5;
 
 %Element 1 - valley
@@ -101,8 +102,35 @@ Normz4 = (0.*t4+g)*m;
 %Element 3 - Loop
 t5 = linspace(0,2*pi,100);
 x5 = lR*sin(t5) + x4(end);
-y5 = (1/(2*pi)).*t5 + y4(end);
+y5 = (1/(pi)).*t5 + y4(end);
 z5 = -lR*cos(t5) + z4(end) + lR;
+
+%Tangential component slope
+Tx5 = lR*cos(t5);
+Ty5 = (1/(pi))*ones(1,length(t5));
+Tz5 = lR*sin(t5);
+Tm5 = (Tx5.^2 + Ty5.^2 + Tz5.^2).^(.5);
+
+
+%Normal acceleration component 
+Nx5 = -lR*sin(t5);
+Ny5 = 0*ones(1,length(t5));
+Nz5 = lR*cos(t5);
+Nm5 = (Nx5.^2 + Ny5.^2 + Nz5.^2).^(.5);
+nx5 = Nx5./Nm5;
+ny5 = Ny5./Nm5;
+nz5 = Nz5./Nm5;
+
+%Normal acceleration of circle
+v5 = getSpeed(z5,h0);
+an5 = v5.^2/lR;
+
+%Normal force components
+theta5 = atan(abs(nz5./nx5));
+Ng5 = g*sin(theta5);
+Normx5 = nx5.*an5*m;
+Normy5 = ny5.*an5*m;
+Normz5 = (nz5.*an5+Ng5)*m;
 
 %Straight section
 t6 = linspace(0,20,100);
@@ -110,22 +138,46 @@ x6 = t6 + x5(end);
 y6 = 0.*t6 + y5(end);
 z6 = 0.*t6 + z5(end);
 
+Normx6 = 0.*t4*m;
+Normy6 = 0.*t4*m;
+Normz6 = (0.*t4+g)*m;
+
 %Banked turn
-t7 = linspace(0,pi/2,50);
+t7 = linspace(0,pi,50);
 x7 = tR*sin(t7) + x6(end);
 y7 = -tR*cos(t7) + y6(end) + tR;
 z7 = 0.*t7 + z6(end);
 
 %Transition
+t8 = linspace(pi/2,5*pi/6,100);
+x8 = r2*cos(t8) + cos(pi/2)*r2 + x7(end);
+y8 = 0.*t8 + y7(end);
+z8 = r2*sin(t8) - sin(pi/2)*r2 + z7(end);
+
 
 %Downward slope
 
+t9 = linspace(0,30,100);
+x9 = -cos(pi/3).*t9 + x8(end);
+y9 = 0.*t9 + y8(end);
+z9 = -sin(pi/3)*t9 + z8(end);
+
 %Transition
+rf = h0-z9(end);
+t10 = linspace(11*pi/6,3*pi/2,100);
+x10 = rf*cos(t10) - cos(11*pi/6)*rf + x9(end);
+y10 = 0.*t10 + y9(end);
+z10 = rf*sin(t10) - sin(11*pi/6)*rf + z9(end);
 
 %Braking zone
-Normxfin = [Normx, Normx2, Normx3, Normx4];
-Normyfin = [Normy, Normy2, Normy3, Normy4];
-Normzfin = [Normz, Normz2, Normz3, Normz4];
+t11 = linspace(0,60,100);
+x11 = -t6 + x10(end);
+y11 = 0.*t6 + y10(end);
+z11 = 0.*t6 + z10(end);
+
+Normxfin = [Normx, Normx2, Normx3, Normx4, Normx5];
+Normyfin = [Normy, Normy2, Normy3, Normy4, Normy5];
+Normzfin = [Normz, Normz2, Normz3, Normz4, Normz5];
 
 %G's
 Gx = Normxfin./(m*g);
@@ -133,9 +185,9 @@ Gy = Normyfin./(m*g);
 Gz = Normzfin./(m*g);
 G = (Gx.^2+Gy.^2+Gz.^2).^(.5);
 
-Xfin = [x,x2,x3,x4,x5,x6,x7];
-Yfin = [y,y2,y3,y4,y5,y6,y7];
-Zfin = [z,z2,z3,z4,z5,z6,z7];
+Xfin = [x,x2,x3,x4,x5,x6,x7,x8,x9,x10,x11];
+Yfin = [y,y2,y3,y4,y5,y6,y7,y8,y9,y10,y11];
+Zfin = [z,z2,z3,z4,z5,z6,z7,z8,z9,z10,z11];
 Vfin = getSpeed(Zfin,h0);
 figure(1);
 colormap(hsv)
